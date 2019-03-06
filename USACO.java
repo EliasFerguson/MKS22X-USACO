cf3f0b18ab42ef779b1552eddef0d7104c357b63
Elias Ferguson <eferguson00@stuy.edu>
2019-03-06 12:25:32 -0500

    solved that problem

--- USACO.java
+++ USACO.java
@@ -7,10 +7,10 @@ public class USACO {
   public static int bronze(String filename) throws FileNotFoundException {
     File in = new File(filename);
     Scanner text = new Scanner(in);
-    int R = parseInt(text.next());
-    int C = parseInt(text.next());
-    int E = parseInt(text.next());
-    int N = parseInt(text.next());
+    int R = Integer.parseInt(text.next());
+    int C = Integer.parseInt(text.next());
+    int E = Integer.parseInt(text.next());
+    int N = Integer.parseInt(text.next());
   }
   public static int silver(String filename) {
     return -1;
