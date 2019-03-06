4552159b428b6f319f44e0540f05f0b89cd51b5f
Elias Ferguson <eferguson00@stuy.edu>
2019-03-06 11:26:57 -0500

    making readIn method to read in the lake

--- USACO.java
+++ USACO.java
@@ -1,6 +1,31 @@
+import java.util.*;
+import java.io.*;
 public class USACO {
-  public static void main(String[] args) {
+  // VARIABLES FOR BRONZE //
+  private static int R;
+  private static int C;
+  private static int[][] lake;
 
+  // VARIABLES FOR SILVER //
+  public static void main(String[] args) {
+    try {
+      readIn("makelake.1.in");
+    }
+    catch (Exception e) {
+      
+    }
+  }
+  public static void readIn(String filename) throws FileNotFoundException {
+    File in = new File(filename);
+    Scanner text = new Scanner(in);
+    List<String> temp = new ArrayList<String>();
+    while (text.hasNextLine()) {
+         String line = text.nextLine();
+         temp.add(line);
+         System.out.println(line);
+    }
+    R = temp.size();
+    C = temp.get(0).length();
   }
   public static int bronze(String filename) {
     return -1;
