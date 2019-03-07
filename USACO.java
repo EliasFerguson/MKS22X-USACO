714cd3754782f98a08f9f2aecc6ad4ac69b0be6e
Elias Ferguson <eferguson00@stuy.edu>
2019-03-07 11:32:03 -0500

    testing lake filling portion of the method

--- USACO.java
+++ USACO.java
@@ -2,7 +2,12 @@ import java.util.*;
 import java.io.*;
 public class USACO {
   public static void main(String[] args) {
-
+    try{
+      bronze("makelake.1.in");
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
   public static int bronze(String filename) throws FileNotFoundException {
     File in = new File(filename);
@@ -16,11 +21,12 @@ public class USACO {
     for (int i = 0; i < R; i++) {
       for (int i2 = 0; i2 < C; i2++) {
         lake[i][i2] = Integer.parseInt(text.next());
+        System.out.print(lake[i][i2] + " ");
       }
     }
     //Done filling.
     //Time to receive the digging instructions, corresponding to i.
-    for (int i = 0; i < N; i++) {
+  /*  for (int i = 0; i < N; i++) {
       int R_s = Integer.parseInt(text.next());
       int C_s = Integer.parseInt(text.next());
       int D_s = Integer.parseInt(text.next());
@@ -28,10 +34,11 @@ public class USACO {
         for (int colModifier = 0; colModifier < 3; colModifier++) {
           int checkRow = rowModifier + R_s;
           int checkCol = colModifier + C_s;
-          lake[checkRow][checkCol] -= D_s; 
+          lake[checkRow][checkCol] -= D_s;
         }
       }
     }
+    */
     return -1;
   }
   public static int silver(String filename) {
